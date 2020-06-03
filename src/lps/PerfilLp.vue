f0f3e1d19b6ca1c813226edf90737a44737b9ccb
Filipe Oliveira <filipeoliveirah@gmail.com>
2020-06-02 22:09:43 -0300

    correções no container do menu

--- src/lps/PerfilLp.vue
+++ src/lps/PerfilLp.vue
@@ -1,104 +1,106 @@
 <template>
 <div v-if="lp" class="lpPerfilDisc over-x-h">
-  <div class="container">
-      <nav
-  :class="{'active': controls.expandMenu, 'scrolled': controls.scroll}"
-  class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="javascript: void(0)">
-      <img 
-      v-if="controls.expandMenu" 
-      class="logo-menu" 
-      src="@/assets/img/solides-header-logo-white.png" 
-      alt="logo da sólides branco">
-      <img v-else class="logo-menu" src="@/assets/img/solides-header-logo.png" alt="logo da sólides">
-    </a>
-    <button @click="togleMenu" class="navbar-toggler no-border" type="button">
-      <img v-if="controls.expandMenu" class="hambmenu" src="@/assets/img/x.png" alt="menu">
-      <img v-else class="hambmenu" src="@/assets/img/hamb-menu.png" alt="menu">
-    </button>
 
-    <div class="collapse navbar-collapse" :class="{'active': controls.expandMenu}">
-      <ul class="navbar-nav">
-        <li @click="togleConteudo(null)"
-        @mouseover="togleConteudo(true)"
-        @mouseout="togleConteudo(false)"
-        class="nav-item">
-          <a class="nav-link p-0" href="javascript: void(0)">
-            Conteúdo
-            <span v-if="controls.expandConteudo" class="submenu">
-              <img class="d-lg-none" src="@/assets/img/seta-pra-cima.png" alt="menu">
-              <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-cima-primary.png" alt="menu">
-            </span>
-            <span v-else class="submenu">
-              <img class="d-lg-none" src="@/assets/img/seta-pra-baixo.png" alt="menu">
-              <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-baixo-primary.png" alt="menu">
-            </span>
-          </a>
-          <div
-          :class="{'active': controls.expandConteudo}"
-          class="dropdown-menu conteudo"
-          aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="https://blog.solides.com.br/">Blog da Sólides</a>
-            <p class="d-none d-lg-block">Os melhores conteúdos para gestão de pessoas</p>
-            <a class="dropdown-item" href="https://www.rhportal.com.br/?__hstc=237478239.26aa389a524d853dd6c37d54678ebce0.1576514278372.1589294299112.1589376437584.128&__hssc=237478239.1.1589376437584&__hsfp=2627390346">RH Portal</a>
-            <p class="d-none d-lg-block">Os melhores conteúdos sobre gestão de pessoas</p>
-            <a class="dropdown-item" href="https://solides.com.br/materiais-educativos/">Materiais</a>
-            <p class="d-none d-lg-block">Os melhores conteúdos para gestão de pessoas</p>
-            <a class="dropdown-item" href="https://contratarerradocustacaro.com.br/?__hstc=237478239.26aa389a524d853dd6c37d54678ebce0.1576514278372.1589294299112.1589376437584.128&__hssc=237478239.1.1589376437584&__hsfp=2627390346">Ferramentas</a>
-            <p class="d-none d-lg-block">Calculadora de rotatividade</p>
-          </div>
-        </li>
-        <li @click="togleCursos(null)"
-        @mouseover="togleCursos(true)"
-        @mouseout="togleCursos(false)"
-        class="nav-item">
-          <a class="nav-link p-0" href="javascript: void(0)">
-            Cursos
-            <span v-if="controls.expandCursos" class="submenu">
-              <img class="d-lg-none" src="@/assets/img/seta-pra-cima.png" alt="menu">
-              <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-cima-primary.png" alt="menu">
-            </span>
-            <span v-else class="submenu">
-              <img class="d-lg-none" src="@/assets/img/seta-pra-baixo.png" alt="menu">
-              <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-baixo-primary.png" alt="menu">
-            </span>
-          </a>
-          <div
-          :class="{'active': controls.expandCursos}"
-          class="dropdown-menu cursos"
-          aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="https://universidade.solides.com.br/course/people-analytics/">Engenharia de Cargos<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
-            <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
-            <a class="dropdown-item " href="https://universidade.solides.com.br/course/people-analytics/">People Analytics<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
-            <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
-            <a class="dropdown-item" href="https://universidade.solides.com.br/course/rh-tecnologico/">RH Tecnológico<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
-            <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
-            <a class="dropdown-item" href="https://universidade.solides.com.br/course/gestao-comportamental-na-pratica/">Recrutamento e Seleção<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
-            <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
-            <a class="dropdown-item" href="https://universidade.solides.com.br/course/gestao-comportamental-na-pratica/">Gestão Comportamental<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
-            <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link p-0" href="#prices">Preços</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link p-0" href="https://materiais.solides.com.br/parceria">Seja Parceiro</a>
-        </li>
-        <li class="nav-item showOnScroll d-none" :class="{'active': controls.scroll}">
-          <a class="nav-link p-0" href="https://checkout.solides.com.br/">
-            <button class="badge badge-pill blue-back color-white two-in-a-row">Comprar Agora</button>
-          </a>
-        </li>
-        <li class="nav-item showOnScroll d-none" :class="{'active': controls.scroll}">
-          <a class="nav-link p-0" href="#form">
-            <button class="badge badge-pill blue-outline two-in-a-row">Fale com um especialista</button>
-          </a>
-        </li>
-      </ul>
+  <nav
+    :class="{'active': controls.expandMenu, 'scrolled': controls.scroll}"
+    class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="javascript: void(0)">
+        <img 
+        v-if="controls.expandMenu" 
+        class="logo-menu" 
+        src="@/assets/img/solides-header-logo-white.png" 
+        alt="logo da sólides branco">
+        <img v-else class="logo-menu" src="@/assets/img/solides-header-logo.png" alt="logo da sólides">
+      </a>
+      <button @click="togleMenu" class="navbar-toggler no-border" type="button">
+        <img v-if="controls.expandMenu" class="hambmenu" src="@/assets/img/x.png" alt="menu">
+        <img v-else class="hambmenu" src="@/assets/img/hamb-menu.png" alt="menu">
+      </button>
+
+      <div class="collapse navbar-collapse" :class="{'active': controls.expandMenu}">
+        <ul class="navbar-nav">
+          <li @click="togleConteudo(null)"
+          @mouseover="togleConteudo(true)"
+          @mouseout="togleConteudo(false)"
+          class="nav-item">
+            <a class="nav-link p-0" href="javascript: void(0)">
+              Conteúdo
+              <span v-if="controls.expandConteudo" class="submenu">
+                <img class="d-lg-none" src="@/assets/img/seta-pra-cima.png" alt="menu">
+                <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-cima-primary.png" alt="menu">
+              </span>
+              <span v-else class="submenu">
+                <img class="d-lg-none" src="@/assets/img/seta-pra-baixo.png" alt="menu">
+                <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-baixo-primary.png" alt="menu">
+              </span>
+            </a>
+            <div
+            :class="{'active': controls.expandConteudo}"
+            class="dropdown-menu conteudo"
+            aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="https://blog.solides.com.br/">Blog da Sólides</a>
+              <p class="d-none d-lg-block">Os melhores conteúdos para gestão de pessoas</p>
+              <a class="dropdown-item" href="https://www.rhportal.com.br/?__hstc=237478239.26aa389a524d853dd6c37d54678ebce0.1576514278372.1589294299112.1589376437584.128&__hssc=237478239.1.1589376437584&__hsfp=2627390346">RH Portal</a>
+              <p class="d-none d-lg-block">Os melhores conteúdos sobre gestão de pessoas</p>
+              <a class="dropdown-item" href="https://solides.com.br/materiais-educativos/">Materiais</a>
+              <p class="d-none d-lg-block">Os melhores conteúdos para gestão de pessoas</p>
+              <a class="dropdown-item" href="https://contratarerradocustacaro.com.br/?__hstc=237478239.26aa389a524d853dd6c37d54678ebce0.1576514278372.1589294299112.1589376437584.128&__hssc=237478239.1.1589376437584&__hsfp=2627390346">Ferramentas</a>
+              <p class="d-none d-lg-block">Calculadora de rotatividade</p>
+            </div>
+          </li>
+          <li @click="togleCursos(null)"
+          @mouseover="togleCursos(true)"
+          @mouseout="togleCursos(false)"
+          class="nav-item">
+            <a class="nav-link p-0" href="javascript: void(0)">
+              Cursos
+              <span v-if="controls.expandCursos" class="submenu">
+                <img class="d-lg-none" src="@/assets/img/seta-pra-cima.png" alt="menu">
+                <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-cima-primary.png" alt="menu">
+              </span>
+              <span v-else class="submenu">
+                <img class="d-lg-none" src="@/assets/img/seta-pra-baixo.png" alt="menu">
+                <img class="d-none d-lg-inline" src="@/assets/img/seta-pra-baixo-primary.png" alt="menu">
+              </span>
+            </a>
+            <div
+            :class="{'active': controls.expandCursos}"
+            class="dropdown-menu cursos"
+            aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="https://universidade.solides.com.br/course/people-analytics/">Engenharia de Cargos<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
+              <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
+              <a class="dropdown-item " href="https://universidade.solides.com.br/course/people-analytics/">People Analytics<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
+              <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
+              <a class="dropdown-item" href="https://universidade.solides.com.br/course/rh-tecnologico/">RH Tecnológico<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
+              <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
+              <a class="dropdown-item" href="https://universidade.solides.com.br/course/gestao-comportamental-na-pratica/">Recrutamento e Seleção<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
+              <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
+              <a class="dropdown-item" href="https://universidade.solides.com.br/course/gestao-comportamental-na-pratica/">Gestão Comportamental<p class="saiba-mais d-lg-none">Saiba mais >></p></a>
+              <p class="saiba-mais d-none d-lg-block">Saiba mais >></p>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-0" href="#prices">Preços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-0" href="https://materiais.solides.com.br/parceria">Seja Parceiro</a>
+          </li>
+          <li class="nav-item showOnScroll d-none" :class="{'active': controls.scroll}">
+            <a class="nav-link p-0" href="https://checkout.solides.com.br/">
+              <button class="badge badge-pill blue-back color-white two-in-a-row">Comprar Agora</button>
+            </a>
+          </li>
+          <li class="nav-item showOnScroll d-none" :class="{'active': controls.scroll}">
+            <a class="nav-link p-0" href="#form">
+              <button class="badge badge-pill blue-outline two-in-a-row">Fale com um especialista</button>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
-  </div>
+
   <div class="container mt-70 pb-60 mt-md-120">
     <div class="row ml-0">
       <div class="text-left color-primary to-left-70 mt-70 discprofiler">

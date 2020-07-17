096382fe33a47b015f47fdd6c9d8cf32cdfa34f9
Vinicius de Santana <vinicius.vts@gmail.com>
2020-07-17 15:06:25 -0300

    fix ultra wide monitors

--- src/components/ComoEstamosAjudando.vue
+++ src/components/ComoEstamosAjudando.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="como-estamos-ajudando">
-    <div class="vertical-pill yellow"></div>
-    <div class="vertical-pill green"></div>
     <div class="container">
+      <div class="vertical-pill yellow"></div>
+      <div class="vertical-pill green"></div>
       <div class="row">
         <div class="col-1 col-lg-4"></div>
         <div class="col-8 col-lg-8">

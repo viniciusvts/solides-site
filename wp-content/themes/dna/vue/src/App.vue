e7618fb9631cbf514714e77ebe464a205cb0f071
Vinicius de Santana <vinicius.vts@gmail.com>
2020-09-04 16:14:22 -0300

    fix: swap font

--- wp-content/themes/dna/vue/src/App.vue
+++ wp-content/themes/dna/vue/src/App.vue
@@ -35,7 +35,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;700;900');
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;700;900&display=swap');
 @import "@/assets/scss/variables.scss";
 *, ::after, ::before{
     font-family: 'Lato', 'Roboto', sans-serif;

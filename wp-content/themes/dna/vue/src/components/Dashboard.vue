e03c4cda7faf62e9fc73611ec2e26247cc054ffc
Vinicius de Santana <vinicius.vts@gmail.com>
2020-09-05 12:56:26 -0300

    fix: remove quote marks

--- wp-content/themes/dna/vue/src/components/Dashboard.vue
+++ wp-content/themes/dna/vue/src/components/Dashboard.vue
@@ -4,14 +4,14 @@
       <div class="container">
         <div class="row">
           <div class="d-lg-none col-12">
-            <h2 class="text-center" v-html="data.titulo"></h2>"
+            <h2 class="text-center" v-html="data.titulo"></h2>
           </div>
           <div class="col-11 col-lg-5 imagem">
             <img :src="data.imagem.url" :alt="data.imagem.alt">
           </div>
           <div class="d-none d-lg-block col-1"></div>
           <div class="col-12 col-lg-6 vertical-center">
-            <h2 class="d-none d-lg-block" v-html="data.titulo"></h2>"
+            <h2 class="d-none d-lg-block" v-html="data.titulo"></h2>
             <div class="text" v-html="data.texto"></div>
           </div>
         </div>
